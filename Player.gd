6f4f7ef0e660e76ef1ca55c9e13bccf8b17f431c
evefeng <wz234ji8@gmail.com>
2021-08-17 23:33:41 +0800

    基本编写完毕

--- Player.gd
+++ Player.gd
@@ -1,21 +1,17 @@
 extends Area2D
 
-signal hit # 敌人碰撞信号
+signal hit
 
-export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+export var speed = 400
+var screen_size
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var velocity = Vector2() # The player's movement vector.
-	
-	# 检查输入
+	var velocity = Vector2()
+
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 	if Input.is_action_pressed("ui_left"):
@@ -24,19 +20,17 @@ func _process(delta):
 		velocity.y += 1
 	if Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
-	
+
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 		$AnimatedSprite.play()
 	else:
 		$AnimatedSprite.stop()
-	
-	# 沿给定方向移动; 更新玩家位置
+
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
 
-	# 播放适当的动画
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_v = false
@@ -45,13 +39,11 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
 
-# 接收信号: 敌人碰撞
 func _on_Player_body_entered(body):
-	hide() # Player disappears after being hit.
+	hide()
 	emit_signal("hit")
 	$CollisionShape2D.set_deferred("disabled", true)
 
-# 重置Player
 func start(pos):
 	position = pos
 	show()
